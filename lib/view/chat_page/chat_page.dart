418e3b9da93012efa169fce1c123cb022dce2682
shifink <muhammadshifin07@gmail.com>
2023-10-02 10:19:52 +0530

    sixth commit

--- lib/view/chat_page/chat_page.dart
+++ lib/view/chat_page/chat_page.dart
@@ -11,9 +11,43 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         elevation: 0,
+        title: Row(
+          children: [
+            BackButton(),
+            CircleAvatar(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "John",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "last seen recently",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 233, 232, 230)),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        actions: [
+          Icon(Icons.call),
+          SizedBox(
+            width: 5,
+          ),
+          Icon(Icons.more_vert_sharp),
+          SizedBox(
+            width: 5,
+          ),
+        ],
       ),
     );
   }
